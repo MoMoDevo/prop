7d4baa7022984553887ab146df5cd737b959a544
MoMoDevo <mohamedhazara87@gmail.com>
2023-05-30 16:34:24 +0200

    pro until t goog auth

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+ 
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+   
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Provider>
+
+          <Nav/>
+         
+          {children}
+          </Provider>
+        </main>
+     
+    </body>
+  </html>
+);
+
+export default RootLayout;
